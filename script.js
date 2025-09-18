b2b6784a387ff11832e6330ab7cf3995288422dc
Corey Ptaszek <7795047+m4rsm4n@users.noreply.github.com>
2025-09-18 12:52:06 -0400

    Create function for computer's random hand choice.

---
+++ script.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+    switch(Math.floor(Math.random() * 3)) {
+        case 0:
+            return 'Rock';
+        case 1:
+            return 'Paper';
+        case 2:
+            return 'Scissors';
+    }
+}
+
+console.log(getComputerChoice());
